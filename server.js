e65ff9bb1b8c67f2a2528e1079214ff844e63386
adi <mihaiadrian1995@gmail.com>
2023-01-06 14:45:49 +0100

    fix typo

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
 })
 
 const server = app.listen(7000, () => {
-  console.log(`Express running -> PORT ${server.address().port)}`);
+  console.log(`Express running -> PORT ${server.address().port}`);
 });
