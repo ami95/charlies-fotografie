736a91cc759ba588174a776741eef9b13a4483f1
adi <mihaiadrian1995@gmail.com>
2023-01-11 10:54:26 +0100

    Add my face

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: "Charlie's Fotografie Wien",
+    title: "Meine ur-leiwande Lebenslauf-Webseite",
     people: people.profiles
   })
 })
